bd69fd2b31b616c1e958bcb5a5d1994256cbd97d
mherreradsci <marcelo.herrerat@gmail.com>
2023-04-16 01:37:43 -0400

    DJ4Z2P-004-Termino - Configuración del  customers/admin. y cambio de TIME_ZONE

--- customers/admin.py
+++ customers/admin.py
@@ -3,4 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Customer
 
-admin.site.register(Customer)
+from django.conf.locale.en import formats as en_formats
+en_formats.DATETIME_FORMAT = "d-m-Y H:i:s"
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'code', 'name', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+    search_fields = ['code', 'name']
+    ordering = ['name', '-id']
+
+
+
+
+admin.site.register(Customer, CustomerAdmin)
