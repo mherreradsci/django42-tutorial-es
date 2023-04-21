952f71cff4fa4bf0835e618340134376f39cb5ae
mherreradsci <marcelo.herrerat@gmail.com>
2023-04-21 18:30:36 -0400

    DJ4Z2P-005-Inicio - actualiza Vista para traer un set de registros desde BD

--- customers/views.py
+++ customers/views.py
@@ -4,9 +4,9 @@ from .models import Customer
 
 
 def index(request):
-    customer_obj = Customer.objects.get(id=2)
-
-    HTML = f'<h1>Customer code: {customer_obj.code}</h1>'
-    HTML += f'<li>Customer name: {customer_obj.name}</li>'
+    customer_list = Customer.objects.all().order_by('-code')
+    HTML = '<h1>Customers List</h1>'
+    for customer_obj in customer_list:
+        HTML += f'<li>{customer_obj.code} - {customer_obj.name}</li>'
     print(HTML)
     return HttpResponse(HTML)
