9130496756a8190e754fa94fcd74dea18ea0d46e
mherreradsci <marcelo.herrerat@gmail.com>
2023-04-22 20:35:33 -0400

    DJ4Z2P-005 - Agrega FVB para CRUD incuida una List. Estandariza nombre de templates

--- customers/urls.py
+++ customers/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 app_name = "customers"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create', views.customer_create, name='customer-create'),
+    path('', views.index, name='customer-index'),
+    path('list', views.customer_list, name='customer-list'),
+    path('new', views.customer_create, name='customer-create'),
+    path('<int:pk>', views.customer_detail, name='customer-detail'),
+    path('<int:pk>/delete', views.customer_delete, name='customer-delete'),
+    path('<int:pk>/update', views.customer_update, name='customer-update'),
 ]
 
