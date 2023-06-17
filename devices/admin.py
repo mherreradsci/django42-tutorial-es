c96f662ba6a68915f566d3713e0e2bf4ad25d8dd
mherreradsci <73266769+mherreradsci@users.noreply.github.com>
2023-06-17 02:31:00 -0400

    DJ4Z2P-019 - Add class MacAddessInline devices/admin.py:

--- devices/admin.py
+++ devices/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
 from .models import Device
+from mac_addresses.models import MacAddress
+
 from django.conf.locale.en import formats as en_formats
 
 en_formats.DATETIME_FORMAT = "d-m-Y H:i:s"
 
 
+class MacAddessInline(admin.TabularInline):
+    model = MacAddress
+    extra = 0
+    fields = [
+        "address",
+        "maad_type",
+        "device",
+        "created_by",
+        "created_at",
+        "updated_by",
+        "updated_at",
+    ]
+    readonly_fields = ("created_by", "created_at", "updated_by", "updated_at")
+
+
 class DeviceAdmin(admin.ModelAdmin):
+    inlines = [MacAddessInline]
     list_display = [
         "id",
         "code",
         "name",
         "active",
+        "uuid",
         "ipv4",
         "ipv6",
         "created_by",
@@ -19,7 +38,7 @@ class DeviceAdmin(admin.ModelAdmin):
         "updated_at",
     ]
 
-    readonly_fields = ("created_by", "created_at", "updated_by", "updated_at")
+    readonly_fields = ("uuid", "created_by", "created_at", "updated_by", "updated_at")
     search_fields = ["code", "name"]
     ordering = ["name", "-id"]
 
