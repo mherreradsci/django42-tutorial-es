310508de0ffa226f7c7e2f3a8f49310b6a9660bf
mherreradsci <73266769+mherreradsci@users.noreply.github.com>
2023-07-06 00:51:00 -0400

    DJ4Z2P-022 - Modifica urls.py para que siempre terminen con /

--- customers/urls.py
+++ customers/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = "customers"
 urlpatterns = [
-    path("list", views.CustomerListView.as_view(), name="list"),
-    path("new", views.CustomerCreateView.as_view(), name="create"),
-    path("<int:pk>", views.CustomerDetailViev.as_view(), name="detail"),
-    path("<int:pk>/delete", views.CustomerDeleteView.as_view(), name="delete"),
-    path("<int:pk>/update", views.CustomerUpdateView.as_view(), name="update"),
+    path("list/", views.CustomerListView.as_view(), name="list"),
+    path("new/", views.CustomerCreateView.as_view(), name="create"),
+    path("<int:pk>/", views.CustomerDetailViev.as_view(), name="detail"),
+    path("<int:pk>/delete/", views.CustomerDeleteView.as_view(), name="delete"),
+    path("<int:pk>/update/", views.CustomerUpdateView.as_view(), name="update"),
 ]
--- dj42_proj/settings.py
+++ dj42_proj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "customers.apps.CustomersConfig",
+    "django_htmx",
     "landing",
     "accounts",
     "common",
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = "dj42_proj.urls"
