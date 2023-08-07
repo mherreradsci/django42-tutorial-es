3033f282a7e4f84531d1e93c0ef9f44550fd8d71
mherreradsci <73266769+mherreradsci@users.noreply.github.com>
2023-08-07 00:30:17 -0400

    DJ4Z2P-026 - test and updates mac_address_types

---
+++ mac_address_types/tests_models.py
@@ -0,0 +1,95 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import MacAddressType
+from accounts.models import User
+
+"""
+Automated Test for MacAddressType Model
+"""
+
+
+from django.utils import timezone
+from unittest import mock
+
+
+class MacAddressTypeTestCase(TestCase):
+    def setUp(self):
+        """
+        Setup
+        """
+        User.objects.create(username="init")
+        self.number_of_mac_address_types = 50
+        self.mocked_created_at = timezone.now()
+        # created_at and updated_at must have the same datetime when the recors
+        # are created
+        with mock.patch(
+            "django.utils.timezone.now", mock.Mock(return_value=self.mocked_created_at)
+        ):
+            for id in range(0, self.number_of_mac_address_types):
+                MacAddressType.objects.create(
+                    code="MC" + str(id).zfill(4),
+                    name="MAC Address Type" + str(id).zfill(4),
+                )
+
+    def test_string_representation(self):
+        object = MacAddressType.objects.first()
+        self.assertEqual(str(object), f"[{object.code}] {object.desc}")
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(
+            str(MacAddressType._meta.verbose_name_plural), "MAC Address Types"
+        )
+
+    def test_query_set_exists(self):
+        qs = MacAddressType.objects.all()
+        self.assertTrue(qs.exists())
+
+    def test_query_counts(self):
+        qs = MacAddressType.objects.all()
+        self.assertEqual(qs.count(), self.number_of_mac_address_types)
+
+    def test_validate_created_at(self):
+        object = MacAddressType.objects.filter(code__exact="MC0000")[0]
+
+        self.assertEqual(
+            self.mocked_created_at, object.created_at + timezone.timedelta(days=0)
+        )
+
+    def test_updated_at(self):
+        object = MacAddressType.objects.order_by("id").last()
+        self.assertEqual(
+            self.mocked_created_at, object.updated_at + timezone.timedelta(days=0)
+        )
+
+    def test_updated_at_and_created_at_must_be_equals(self):
+        qs = MacAddressType.objects.all()
+        for c in qs:
+            self.assertEqual(c.created_at, c.updated_at)
+
+    def test_updated_at_after_update(self):
+        object = MacAddressType.objects.order_by("id")[0]
+        object.name = "New name"
+        object.save()
+
+        self.assertLess(object.created_at, object.updated_at)
+
+    def test_get_absolute_url(self):
+        object = MacAddressType.objects.filter(code__exact="MC0000")[0]
+
+        self.assertEqual(object.get_absolute_url(), reverse("mac_address_types:list"))
+
+    # def test_get_macaddress(self):
+    #     object = MacAddressType.objects.filter(code__exact="DV0000")[0]
+    #     self.assertEqual(
+    #         object.get_macaddress().count(), 0)
+
+    # def test_natural_key(self):
+    #     object = MacAddressType.objects.filter(code__exact="DV0000")[0]
+    #     self.assertEqual(
+    #         object.natural_key(), "DV0000")
+
+    # def test_get_by_natural_key(self):
+    #     mac_address_type = MacAddressType.objects.get_by_natural_key(
+    #         code="DV0000")
+    #     self.assertEqual(mac_address_type.id, 1)
---
+++ mac_address_types/tests_views.py
@@ -0,0 +1,119 @@
+from accounts.models import User
+from django.test import TestCase, Client, RequestFactory
+from django.urls import reverse
+from .models import MacAddressType
+
+
+class MacAddressTypeTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.factory = RequestFactory()
+        cls.client = Client()
+
+        # Create an user for login
+        cls.user = User.objects.create_user(username="testuser", password="password")
+
+        cls.number_of_mac_address_types = 50
+
+        for id in range(0, cls.number_of_mac_address_types):
+            MacAddressType.objects.create(
+                code="MA" + str(id).zfill(4), name="MacAddressType " + str(id).zfill(4)
+            )
+
+    def test_macaddresstype_list_view_get_queryset(self):
+        result = self.client.login(username="testuser", password="password")
+
+        headers = {"HTTP_HX-Request": "true"}
+        response = self.client.get("/mac_address_types/list/", **headers)
+        self.assertEqual(response.status_code, 200)  # check 200 OK response
+
+        clients = response.context
+
+        self.assertEqual(
+            len(clients["macaddresstype_list"]), clients["view"].paginate_by
+        )
+
+    def test_macaddresstype_list_view_get_queryset_filtered(self):
+        result = self.client.login(username="testuser", password="password")
+        self.assertTrue(result)
+        response = self.client.get("/mac_address_types/list/?q=0000")
+
+        self.assertEqual(
+            response.template_name, "mac_address_types/macaddresstype_list.html"
+        )
+        self.assertEqual(response.context_data["paginator"].count, 1)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_mac_address_type_create_view_get_initial(self):
+        result = self.client.login(username="testuser", password="password")
+        self.assertTrue(result)
+
+        url = reverse("mac_address_types:create")
+        form_data = {"code": "NEW", "name": "New MacAddressType"}
+        response = self.client.post(path=url, data=form_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/mac_address_types/list/")
+
+    def test_mac_address_type_update_view(self):
+        result = self.client.login(username="testuser", password="password")
+        self.assertTrue(result)
+
+        object = MacAddressType.objects.all().first()
+
+        url = reverse("mac_address_types:update", kwargs={"pk": object.pk})
+
+        form_data = {
+            "code": object.code,
+            "name": object.name,
+            "active": object.active,
+            "active_from": object.active_from,
+            "active_until": object.active_until,
+            "created_by": self.user,
+            "updated_by": self.user,
+        }
+
+        response = self.client.post(path=url, data=form_data)
+        self.assertEqual(response.status_code, 302)
+
+    def test_mac_address_type_delete_view_get_request(self):
+        result = self.client.login(username="testuser", password="password")
+        self.assertTrue(result)
+
+        qs = MacAddressType.objects.all()
+        object = qs.first()
+
+        url = reverse("mac_address_types:delete", args=(object.id,))
+
+        response = self.client.get(url, follow=True)
+
+        self.assertContains(response, "Are you sure you want to delete ")
+        self.assertEqual(
+            response.template_name[0],
+            "mac_address_types/macaddresstype_confirm_delete.html",
+        )
+        self.assertEqual(response.status_code, 200)
+
+        post_response = self.client.post(url, data={"cancel": True}, follow=True)
+
+        self.assertEqual(post_response.status_code, 200)
+        self.assertEqual(
+            post_response.template_name, "mac_address_types/macaddresstype_list.html"
+        )
+
+    def test_mac_address_type_delete_view_post_request(self):
+        result = self.client.login(username="testuser", password="password")
+        self.assertTrue(result)
+
+        object = MacAddressType.objects.all().first()
+        url = reverse("mac_address_types:delete", args=(object.id,))
+
+        post_response = self.client.post(url, follow=True)
+        self.assertEqual(post_response.status_code, 200)
+        self.assertEqual(
+            post_response.template_name, "mac_address_types/macaddresstype_list.html"
+        )
+        self.assertEqual(
+            len(post_response.context["macaddresstype_list"]),
+            post_response.context["view"].paginate_by,
+        )
