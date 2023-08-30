f468720f863d7e324532a68a9db38e57f2e3004f
mherreradsci <73266769+mherreradsci@users.noreply.github.com>
2023-08-30 01:00:40 -0400

    DJ4Z2P-027 - Modify alerts.js for fadeout more than one alert

--- static/js/alerts.js
+++ static/js/alerts.js
@@ -1,5 +1,10 @@
+
 $(document).ready(function () {
-    $("#success-alert").fadeTo(5000, 500).slideUp(500, function () {
-        $("#success-alert").slideUp(500);
-    });
+    alerts = document.querySelectorAll('[id^="success-alert"]')
+    for (let i = 0; i < alerts.length; i++) {
+        a = alerts[i].id
+        $(`#${a}`).fadeTo(5000, 500).slideUp(500, function () {
+            $(`#${a}`).slideUp(500);
+        });
+    }
 });
